744e40d154e8386a0345712e0dd6cc84a96f3525
Stéphane Mbaye <stephane.mbaye@gmail.com>
2015-04-10 06:12:59 +0200

    Version for the DHuS Workshop #2 demo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,4 @@
-
 document.addEventListener('polymer-ready', function() {
-
 
 function stringFixedSize(string, maxLength)
 {
@@ -23,38 +21,84 @@ function stringFixedSize(string, maxLength)
 function parseSearchResult(inputData)
 {
    var data = [];
+
+   if (inputData == null)
+   return data;
+
    for (var i = 0; i < inputData.length; i++)
    {
       data.push({
          title: stringFixedSize(inputData[i].title, 28),
-         thumbnail: 'http://dhus.gael.fr/dhus/odata/v1/Products(\'' + inputData[i].id + '\')/Products(\'Thumbnail\')/$value'
+         thumbnail: '/dhus/odata/v1/Products(\'' + inputData[i].id + '\')/Products(\'Thumbnail\')/$value'
       });
    }
    return data;
 }
 
+
+var app = document.querySelector('#app');
+
+
 var searchAjax = document.querySelector('#searchResults');
 
 searchAjax.addEventListener('core-response',function(e) {
     var useE = e;
-    var app = document.querySelector('#app');
     app.data = parseSearchResult(this.response.feed.entry);
-    
-    
 });
 
+function processSearch(value)
+{
+	   console.log(value);
+}
+
+app.collections = [
+                  'All Collections',
+                  'Sentinel 1',
+                  'Sentinel 2',
+                  'Sentinel 3 - OLCI',
+                  'Sentinel 3 - SLSTR'
+                ];
+
+
+app.bulkActions = [
+                   'Select All',
+                   'Download Now',
+                   'Add to Cart',
+                   'Transform...',
+                   'Delete'
+                 ];
+
+//Polymer('searchResults',
+//{
+//   handleResponse: function(e)
+//   {
+//      console.log(e);
+//      this.$.app.data = parseSearchResult(this.response.feed.entry);
+//  }
+//}); 
+//
+//Polymer('search-input',
+//{
+//   onKeyUp: function(e)
+//   {
+//      console.log('Search rocks: ' + this.value);
+//      if (e.keyCode === 13) // Enter
+//      {
+//         this.$.searchResults.params = this.value;
+//      }
+//   },
+//   onClick: function(e)
+//   {
+//      console.log(e);
+//      console.log('Search click rocks: ' + this.value); }
+//   }
+//);
 
 // Listen for template bound event to know when bindings
 // have resolved and content has been stamped to the page
+
 app.addEventListener('template-bound', function() {
   console.log('DHuS - NeoUI app is ready to rock!');
 });
 
-
-
 });
-
-
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-//})(wrap(document));
